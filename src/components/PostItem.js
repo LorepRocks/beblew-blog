44f3cb0b72f2afef63367ad88292b42b53f3459b
LorepRocks <lorenajrc@gmail.com>
2020-07-25 22:42:32 -0500

    post cards design was changed

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -6,38 +6,39 @@ import '../styles/shared.css';
 function PostItem(props) {
   const post = props.post;
   console.log('___post', post);
+  const bgStyle = {
+    backgroundImage: `url(${post.feature_image.replace(
+      'localhost',
+      '192.168.0.15'
+    )})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+  };
   return (
-    <article className='post shadow large'>
-      <div className='post__header'>
-        <div className='post__options'>
-          <div className='post__options_save '></div>
-          <div className='post__options_share'></div>
-          <div className='post__options_pin'></div>
-        </div>
-      </div>
-      <Link to={`post/${post.slug}`} className='redirect'>
-        <div className='post__info_container'>
-          <div className='post__title'>
-            <div className='post__title_box'>{post.title}</div>
-            <span></span>
-          </div>
-          <div className='post__item'>
-            <div className='post__info'>
-              <blockquote className='post__author'>
-                {post.primary_author.name}
-              </blockquote>
-              <div className='post__tags'>
-                {post.tags.map((tag) => (
-                  <div key={tag.id} className='post__tag'>
-                    {tag.name}
-                  </div>
-                ))}
-              </div>
+    <article style={bgStyle} className=' post shadow large'>
+      <div className='bg-gradient'>
+        <Link to={`post/${post.slug}`} className='redirect'>
+          <div className='post__info_container'>
+            <div className='post__title'>
+              <div className='post__title_box'>{post.title}</div>
+              <span></span>
             </div>
-            <div className='post__preview'>{post.excerpt}</div>
+            <div className='post__item'>
+              <div className='post__info'>
+                <div className='post__tags'>
+                  {post.tags.map((tag) => (
+                    <div key={tag.id} className='post__tag'>
+                      {tag.name}
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className='post__preview'>{post.excerpt}</div>
+            </div>
           </div>
-        </div>
-      </Link>
+        </Link>
+      </div>
     </article>
   );
 }
