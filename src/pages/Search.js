10b9862cb28021f5ec2b21ebc1e3fde91c3d3e24
LorepRocks <lorenajrc@gmail.com>
2020-08-06 22:23:19 -0500

    implement react hooks for search component

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,115 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/shared.css';
 import SearchResult from '../components/SearchResult';
 
 import Api from '../Api';
-class Search extends React.Component {
-  constructor(props) {
-    console.log('___search props', props);
-    super();
-    this.state = {
-      tags: [],
-      postByTag: [],
-      searchText: '',
-      posts: props.posts,
-      filteredPosts: [],
-      onClose: props.onSearchClick,
-    };
-  }
-  componentDidMount() {
-    this.api = new Api();
-    this.getTags();
+function Search(props) {
+  const [tags, setTags] = React.useState([]);
+  const [postByTag, setPostByTag] = React.useState([]);
+  const [filteredPosts, setFilteredPost] = React.useState([]);
+  const api = new Api();
+
+  useEffect(() => {
+    console.log('___get tags');
+    getTags();
+    /* FIXME: should exist a better way to ensure don't allow scroll on search modal without add position to body */
     document.body.style.position = 'fixed';
+  }, []);
+
+  async function getTags() {
+    await api.getTags().then((tags) => {
+      setTags(tags);
+    });
+  }
+  async function handleOnTagClick(e) {
+    const tagSlug = e.target.id;
+    await api.getPostsByTag(tagSlug).then((posts) => {
+      setPostByTag(posts);
+    });
   }
 
-  getTags = async () => {
-    console.log('____getting tags');
-    await this.api.getTags().then((tags) => {
-      console.log('__tags', tags);
-      this.setState({
-        tags: tags,
-      });
-    });
-  };
-  handleOnTagClick = async (e) => {
-    console.log('__selected tag', e.target.id);
-    const tagSlug = e.target.id;
-    await this.api.getPostsByTag(tagSlug).then((posts) => {
-      console.log(posts);
-      this.setState({
-        postByTag: posts,
-      });
-    });
-    console.log('___post by tag', this.state.postByTag);
-  };
-
-  handleChangeText = (e) => {
-    console.log('__text', e.target.value);
+  async function handleChangeText(e) {
     let query = e.target.value;
     const regex = /^ *$/g;
-    this.setState({
-      filteredPosts: [],
-      postByTag: [],
-    });
-    console.log('_____filtered befre', this.state.filteredPosts);
+    setFilteredPost([]);
+    setPostByTag([]);
 
     if (query !== '' && !regex.test(query)) {
-      let filter = this.state.posts.filter((post) => {
+      let filter = props.posts.filter((post) => {
         return post.title.toLowerCase().includes(query.toLowerCase());
       });
-      this.setState({
-        filteredPosts: filter,
-      });
-      console.log('_____filtered', this.state.filteredPosts);
+      setFilteredPost(filter);
     }
-  };
-
-  render() {
-    return (
-      <div className={`search-container ${this.props.addClass}`}>
-        <div onClick={this.props.onClose} className='close'></div>
-        <div className='search-input-container'>
-          <input
-            onChange={this.handleChangeText}
-            className='search-input'
-            placeholder='Buscar un post...'
-          />
-        </div>
-        {this.state.postByTag.length === 0 &&
-          this.state.filteredPosts.length === 0 && (
-            <div className='search-results'>
-              <ul
-                onClick={this.handleOnTagClick}
-                className='search-tags-container'
-              >
-                {this.state.tags.map((tag) => (
-                  <li className='search-tags-item' key={tag.id} id={tag.slug}>
-                    {tag.name}
-                  </li>
-                ))}
-              </ul>
-              <div className='tags-msg'>
-                Puedes selecionar un tag para filtrar
-              </div>
-              <div className='result-count'>0 Post Encontrados</div>
-            </div>
-          )}
-        {this.state.postByTag.length > 0 &&
-          this.state.filteredPosts.length === 0 && (
-            <SearchResult
-              posts={this.state.postByTag}
-              onClose={this.state.onClose}
-            />
-          )}
-        {this.state.filteredPosts.length > 0 && (
-          <SearchResult
-            posts={this.state.filteredPosts}
-            onClose={this.state.onClose}
-          />
-        )}
-      </div>
-    );
   }
+
+  return (
+    <div className={`search-container ${props.addClass}`}>
+      <div onClick={props.onClose} className='close'></div>
+      <div className='search-input-container'>
+        <input
+          onChange={handleChangeText}
+          className='search-input'
+          placeholder='Buscar un post...'
+        />
+      </div>
+      {postByTag.length === 0 && filteredPosts.length === 0 && (
+        <div className='search-results'>
+          <ul onClick={handleOnTagClick} className='search-tags-container'>
+            {tags.map((tag) => (
+              <li className='search-tags-item' key={tag.id} id={tag.slug}>
+                {tag.name}
+              </li>
+            ))}
+          </ul>
+          <div className='tags-msg'>Puedes selecionar un tag para filtrar</div>
+          <div className='result-count'>0 Post Encontrados</div>
+        </div>
+      )}
+      {postByTag.length > 0 && filteredPosts.length === 0 && (
+        <SearchResult posts={postByTag} onClose={props.onClose} />
+      )}
+      {filteredPosts.length > 0 && (
+        <SearchResult posts={filteredPosts} onClose={props.onClose} />
+      )}
+    </div>
+  );
 }
 
 export default Search;
